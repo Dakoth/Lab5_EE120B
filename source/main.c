90a37191cfc7d22320b13216d23a3c487a62eb73
Alfredo Gonzalez <alfredogonzalez1999@gmail.com>
2020-04-25 22:50:21 -0700

    Copy/pasted Lab 4 part 2 code for part 2

--- source/main.c
+++ source/main.c
@@ -1,74 +1,123 @@
-/*	Author: Alfredo Gonzalez
- *  Partner(s) Name: 
- *	Lab Section: 028
- *	Assignment: Lab # 5  Exercise #1
+/*	Author: agonz250
+ *  Partner(s) Name: 028 
+ *	Lab Section:
+ *	Assignment: Lab # 5  Exercise # 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
- *	LINK: 
  *	code, is my own original work.
+ *	LINK: 
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+enum States {Start, Wait, incHold, decHold, reset} state; 
+
+unsigned char tmpA; //global variables 
+unsigned char tmpC;//= 0x00; 
+
+void Tick() {
+	//unsigned char tmpA = PINA;
+	switch(state) { //transitions
+		case Start:
+			tmpC = 0; //Originally used to be 7 in lab 4 pt 2
+			state = Wait;
+			break;
+		
+		case Wait:
+			if ( ((tmpA & 0x01) == 0x01) && ((tmpA & 0x02) == 0x00) ) { //if PA0 && !PA1
+				state = incHold;
+
+				if (tmpC < 9) { ++tmpC; } //TRANSITION ACTION
+
+			} 
+			else if ( ((tmpA & 0x02) == 0x02) && ((tmpA & 0x01) == 0x00) ) //if PA1 && !PA0
+			{
+				state = decHold;
+
+				if (tmpC > 0) { --tmpC; }	//TRANSITION ACTION
+
+			}
+			else if ((tmpA & 0x03) == 0x03) { //if PA1 && PA0
+				state = reset;
+				tmpC = 0;
+			}
+			else { //If none of the above
+				state = Wait;
+			}
+			break;
+
+		case incHold:
+			if ((tmpA & 0x01) == 0x01) { 
+				state = incHold;
+			}
+			else { 
+				state = Wait;
+			}			
+			break;
+
+		case decHold:
+			if ((tmpA & 0x02) == 0x02) {
+				state = decHold;
+			}
+			else {
+				state = Wait;
+			}
+			break;
+
+		case reset: 
+			if ((tmpA & 0x03) == 0x03) {
+				state = reset;
+			}
+			else {
+				state = Wait;
+			}
+			break;
+
+		default: 
+			//shouldn't go here
+			state = Start;
+			break;					
+	}
+
+	switch(state) { 
+		case Wait:
+			break;
+
+		case incHold:
+			//if (tmpC < 9) { ++tmpC; }
+			break;
+		
+		case decHold:
+			//if (tmpC > 0) { --tmpC; }
+			break;
+		
+		case reset: 
+			//tmpC = 0;
+			break;
+		default:
+			break;
+	}
+}
+
 int main(void) {
-/* Insert DDR and PORT initializations */
-/* Insert DDR and PORT initializations */
-	DDRA = 0x00; PORTA = 0xFF;	
-	//Makes all port A's input pins as inputs; initalized to 1s
-	DDRC = 0xFF; PORTC = 0x00;	
-	//Makes all port B's input pins as outputs; initalized to 0s
-
-	unsigned char tmpA = 0x00; 
-	unsigned char tmpC = 0x00;
-
+    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; //Makes all As as input
+	DDRC = 0xFF; PORTC = 0x00; //Makes all C pins as output	
+	
+	tmpC = 0x00;
+	state = Start;  
 	/* Insert your solution below */
     	while (1) {
-		//1.)read inputi
-		tmpA = ~PINA; //change this back to ~PINA after testing 
-		
-		//2.) perform computation
-		//because A only has 4 bits, should clear them to zero in case
-		if ( (tmpA & 0x0F)  > 0) { //If 1 or 2, PC5 lights up  
-			tmpC = tmpC | 0x20; 
-		}
-	       	//tmpA = ~PINA;	
-		if ( (tmpA & 0x0F) >= 3 ) { //if 3 or 4, PC5 and PC4 light up  
-			tmpC = tmpC | 0x10;
-		}
-		//tmpA = ~PINA;
-		if ( (tmpA & 0x0F)  >= 5 ) { //if 5 or 6, PC5-PC3 on 
-			tmpC = tmpC | 0x08;
-		}
-		//tmpA = ~PINA;
-		if ( (tmpA & 0x0F) >= 7 ) { //7-9, PC5-PC2
-			tmpC = tmpC | 0x04;
-		}
-		//tmpA = ~PINA;
-		if ( (tmpA & 0x0F)  >= 10 ) { //10-12, PC5-PC1
-			tmpC = tmpC | 0x02;
-		}
-		//tmpA = ~PINA;
-		if ( (tmpA & 0x0F) >= 13 ) { //13-15, PC5-PC0
-			tmpC = tmpC |  0x01;
-		}
-		
-		//If tmp < 5,, light up PC6
-		//tmpA = ~PINA;			
-		if( (tmpA & 0x0F) < 5 ) { 
-			tmpC = tmpC | 0x40;
-		}
-		else {	//Else keep everything the same 
-			tmpC = tmpC & 0xBF;
-		} 
-		
-		//3.) Write Output 
-		PORTC = tmpC;
+		//read inputs
+		tmpA = ~PINA; 
 
-		tmpC = 0x00; //reset the tmp variables
-		tmpA = 0x00;
+		Tick(); 
+	
+		//write output
+		PORTC = tmpC;		
     	}
     	return 1;
 }
