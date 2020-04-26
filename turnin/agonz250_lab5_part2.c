cf2a5f8b8c64833f62dfc8abc3d0e8b3a51d128d
Alfredo Gonzalez <56003346+Dakoth@users.noreply.github.com>
2020-04-26 02:19:29 -0700

    Added part 2 demo

--- turnin/agonz250_lab5_part2.c
+++ turnin/agonz250_lab5_part2.c
@@ -6,7 +6,7 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
- *	LINK: 
+ *	VIDEO LINK: https://drive.google.com/file/d/1i02-0ruKXY4dgQuf6etja3hH_uKizyQV/view?usp=sharing
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
